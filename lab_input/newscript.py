c5a84b622de4b1c0ec67d76bd70a8709bb411bf7
kin19717 <kin19717@gmail.com>
2022-05-24 19:11:41 +0000

    mixed lists

---
+++ lab_input/newscript.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python3 
+
+def main():
+    my_name = input("What is my name? ")
+    print("Welcome", my_name)
+main()
---
+++ mix-list/mixlist01_challenge01.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+
+# create a list containing three items
+iplist = [ 5060, "80", 55, "10.0.0.1", "10.20.30.1", "ssh" ]
+
+
+# display only the IP address
+print(iplist[3], iplist[4] )
+
+
